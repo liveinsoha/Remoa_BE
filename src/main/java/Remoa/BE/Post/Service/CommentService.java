3b7154e74235f1b2f282508cf36fbe8353719503
MIN0225 <cro77@naver.com>
2023-03-12 14:10:15 +0900

    fix: comment&feedback 예외처리추가

--- src/main/java/Remoa/BE/Post/Service/CommentService.java
+++ src/main/java/Remoa/BE/Post/Service/CommentService.java
@@ -11,10 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -50,15 +52,24 @@ public class CommentService {
     }
 
     @Transactional
-    public void registerComment(Member member, String comment, Long postId){
+    public Post registerComment(Member member, String comment, Long postId){
         Comment commentObj = new Comment();
         String formatDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        Post post = postRepository.findByPostId(postId); // 댓글을 작성하는 게시글
-        commentObj.setPost(post);
+        //Post post = postRepository.findByPostId(postId); // 댓글을 작성하는 게시글
+        Optional<Post> post = postRepository.findOne(postId);
+        Post myPost; // 댓글을 등록할 게시물
+        if(post.isPresent()){
+            myPost = post.get();
+        } else{
+            myPost = post.orElseThrow(() -> new IllegalStateException("값이 없습니다."));
+        }
+
+        commentObj.setPost(myPost);
         commentObj.setMember(member);
         commentObj.setComment(comment);
         commentObj.setCommentedTime(formatDate);
         commentRepository.saveComment(commentObj);
 
+        return post.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found"));
     }
 }
--- src/main/java/Remoa/BE/Post/Service/FeedBackService.java
+++ src/main/java/Remoa/BE/Post/Service/FeedBackService.java
@@ -2,6 +2,7 @@ package Remoa.BE.Post.Service;
 
 import Remoa.BE.Member.Domain.Feedback;
 import Remoa.BE.Member.Domain.Member;
+import Remoa.BE.Post.Domain.Post;
 import Remoa.BE.Post.Repository.FeedbackRepository;
 import Remoa.BE.Post.Repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,13 +22,23 @@ public class FeedBackService {
     private final FeedbackRepository feedbackRepository;
     private  final PostRepository postRepository;
 
+
+
     @Transactional
     public void registFeedback(Member member, String feedback, Long postId, Integer pageNumber){
 
         Feedback feedbackObj = new Feedback();
+        Optional<Post> post = postRepository.findOne(postId);
+        Post myPost; // feedback을 등록할 게시물
+        if(post.isPresent()){
+            myPost = post.get();
+        } else{
+            myPost = post.orElseThrow(() -> new IllegalStateException("값이 없습니다."));
+        }
+
 
         String formatDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        feedbackObj.setPost(postRepository.findByPostId(postId));
+        feedbackObj.setPost(myPost);
         feedbackObj.setMember(member);
         feedbackObj.setPageNumber(pageNumber);
         feedbackObj.setFeedback(feedback);
