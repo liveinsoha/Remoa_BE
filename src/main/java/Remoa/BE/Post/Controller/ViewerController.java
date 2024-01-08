fed95ad5f93746aaddb0980d7d3412a50e52d063
jayrhie <vhy97z@naver.com>
2024-01-08 21:27:04 +0900

    fix: 레퍼런스 상세보기(ViewerController)파일 가독성 향상

--- src/main/java/Remoa/BE/Post/Controller/ViewerController.java
+++ src/main/java/Remoa/BE/Post/Controller/ViewerController.java
@@ -1,5 +1,7 @@
 package Remoa.BE.Post.Controller;
 
+import Remoa.BE.Member.Domain.Comment;
+import Remoa.BE.Member.Domain.Feedback;
 import Remoa.BE.Member.Domain.Member;
 import Remoa.BE.Member.Dto.Res.ResMemberInfoDto;
 import Remoa.BE.Member.Service.FollowService;
@@ -57,43 +59,72 @@ public class ViewerController {
         Post post = postService.findOneViewPlus(referenceId);
 
         // 조회한 post의 comment(대댓글 제외) 조회 및 CommentDto로 매핑 -> 이후 주석에서 대댓글 comment 조회
-        List<ResCommentDto> comments = commentService.findAllCommentsOfPost(post).stream()
-                .filter(comment -> comment.getParentComment() == null)
-                .map(comment -> ResCommentDto.builder()
-                        .commentId(comment.getCommentId())
-                        .isLiked(finalMyMemberId != null && commentService.findCommentLike(finalMyMemberId, comment.getCommentId()).isPresent())
-                        .member(new ResMemberInfoDto(comment.getMember().getMemberId(),
-                                comment.getMember().getNickname(),
-                                comment.getMember().getProfileImage(),
-                                finalMyMember != null ? followService.isMyMemberFollowMember(finalMyMember, post.getMember()) : null))
-                        .comment(comment.getComment())
-                        .likeCount(comment.getCommentLikeCount())
-                        .commentedTime(comment.getCommentedTime())
-                        //아래부터 대댓글 comment 조회 및 dto 매핑
-                        .replies(commentService.getParentCommentsReply(comment).stream()
-                                .map(reply -> ResReplyDto.builder()
-                                        .replyId(reply.getCommentId())
-                                        .member(new ResMemberInfoDto(reply.getMember().getMemberId(),
-                                                reply.getMember().getNickname(),
-                                                reply.getMember().getProfileImage(),
-                                                finalMyMember != null ? followService.isMyMemberFollowMember(finalMyMember, post.getMember()) : null))
-                                        .content(reply.getComment())
-                                        .likeCount(reply.getCommentLikeCount())
-                                        .isLiked(finalMyMemberId != null && commentService.findCommentLike(finalMyMemberId, reply.getCommentId()).isPresent())
-                                        .repliedTime(reply.getCommentedTime())
-                                        .build()).collect(Collectors.toList()))
-                        .build()).collect(Collectors.toList());
+        List<ResCommentDto> comments = commentList(post, myMember);
 
         // 조회한 post의 feedback(대댓글 제외) 조회 및 FeedbackDto로 매핑 -> 이후 주석에서 대댓글 feedback 조회
-        List<ResFeedbackDto> feedbacks = feedbackService.findAllFeedbacksOfPost(post).stream()
+        List<ResFeedbackDto> feedbacks = feedbackList(post, myMember);
+
+        List<String> fileNames = post.getUploadFiles() != null ?
+                post.getUploadFiles().stream().map(UploadFile::getStoreFileUrl).collect(Collectors.toList()) : null;
+
+        // 위에 생성한 CommentDto, FeedbackDto를 이용해 ReferenceViewerDto 매핑.
+        ResReferenceViewerDto result = ResReferenceViewerDto.builder()
+                .postId(post.getPostId())
+                .postMember(new ResMemberInfoDto(post.getMember().getMemberId(),
+                        post.getMember().getNickname(),
+                        post.getMember().getProfileImage(),
+                        isMymMemberFollowMember(finalMyMember, post.getMember())))
+                .thumbnail(post.getThumbnail().getStoreFileUrl())
+                .contestName(post.getContestName())
+                .contestAwardType(post.getContestAwardType())
+                .category(post.getCategory().getName())
+                .title(post.getTitle())
+                .likeCount(post.getLikeCount())
+                .isLiked(isLikedPost(myMember, post))
+                .scrapCount(post.getScrapCount())
+                .isScraped(isScrapedPost(myMember, post))
+                .postingTime(post.getPostingTime().toString())
+                .views(post.getViews())
+                .youtubeLink(post.getYoutubeLink())
+                .pageCount(post.getPageCount())
+                .fileNames(fileNames)
+                .comments(comments)
+                .feedbacks(feedbacks)
+                .build();
+
+        return successResponse(CustomMessage.OK, result);
+    }
+
+    private Boolean isMymMemberFollowMember(Member myMember, Member member) {
+        return myMember != null ? followService.isMyMemberFollowMember(myMember, member) : null;
+    }
+
+    private Boolean isLikedPost(Member myMember, Post post) {
+        return myMember != null && postService.isThisPostLiked(myMember, post);
+    }
+
+    private Boolean isScrapedPost(Member myMember, Post post) {
+        return myMember != null && postService.isThisPostScraped(myMember, post);
+    }
+
+    private Boolean isLikedComment(Long myMemberId, Comment comment) {
+        return myMemberId != null && commentService.findCommentLike(myMemberId, comment.getCommentId()).isPresent();
+    }
+
+    private Boolean isLikedFeedback(Long myMemberId, Feedback feedback) {
+        return myMemberId != null && feedbackService.findFeedbackLike(myMemberId, feedback.getFeedbackId()).isPresent();
+    }
+
+    private List<ResFeedbackDto> feedbackList(Post post, Member myMember) {
+        return feedbackService.findAllFeedbacksOfPost(post).stream()
                 .filter(feedback -> feedback.getParentFeedback() == null)
                 .map(feedback -> ResFeedbackDto.builder()
                         .feedbackId(feedback.getFeedbackId())
-                        .isLiked(finalMyMemberId != null && feedbackService.findFeedbackLike(finalMyMemberId, feedback.getFeedbackId()).isPresent())
+                        .isLiked(isLikedFeedback(myMember.getMemberId(), feedback))
                         .member(new ResMemberInfoDto(feedback.getMember().getMemberId(),
                                 feedback.getMember().getNickname(),
                                 feedback.getMember().getProfileImage(),
-                                finalMyMember != null ? followService.isMyMemberFollowMember(finalMyMember, post.getMember()) : null))
+                                isMymMemberFollowMember(myMember, post.getMember())))
                         .feedback(feedback.getFeedback())
                         .page(feedback.getPageNumber())
                         .likeCount(feedback.getFeedbackLikeCount())
@@ -105,42 +136,41 @@ public class ViewerController {
                                         .member(new ResMemberInfoDto(reply.getMember().getMemberId(),
                                                 reply.getMember().getNickname(),
                                                 reply.getMember().getProfileImage(),
-                                                finalMyMember != null ? followService.isMyMemberFollowMember(finalMyMember, post.getMember()) : null))
+                                                isMymMemberFollowMember(myMember, post.getMember())))
                                         .content(reply.getFeedback())
                                         .likeCount(reply.getFeedbackLikeCount())
-                                        .isLiked(finalMyMemberId != null && feedbackService.findFeedbackLike(finalMyMemberId, reply.getFeedbackId()).isPresent())
+                                        .isLiked(isLikedFeedback(myMember.getMemberId(), reply))
                                         .repliedTime(reply.getFeedbackTime())
                                         .build()).collect(Collectors.toList()))
                         .build()).collect(Collectors.toList());
+    }
 
-        List<String> fileNames = post.getUploadFiles() != null ?
-                post.getUploadFiles().stream().map(UploadFile::getStoreFileUrl).collect(Collectors.toList()) : null;
-
-        // 위에 생성한 CommentDto, FeedbackDto를 이용해 ReferenceViewerDto 매핑.
-        ResReferenceViewerDto result = ResReferenceViewerDto.builder()
-                .postId(post.getPostId())
-                .postMember(new ResMemberInfoDto(post.getMember().getMemberId(),
-                        post.getMember().getNickname(),
-                        post.getMember().getProfileImage(),
-                        finalMyMember != null ? followService.isMyMemberFollowMember(finalMyMember, post.getMember()) : null))
-                .thumbnail(post.getThumbnail().getStoreFileUrl())
-                .contestName(post.getContestName())
-                .contestAwardType(post.getContestAwardType())
-                .category(post.getCategory().getName())
-                .title(post.getTitle())
-                .likeCount(post.getLikeCount())
-                .isLiked(finalMyMemberId != null && postService.isThisPostLiked(myMember, post))
-                .scrapCount(post.getScrapCount())
-                .isScraped(finalMyMemberId != null && postService.isThisPostScraped(myMember, post))
-                .postingTime(post.getPostingTime().toString())
-                .views(post.getViews())
-                .youtubeLink(post.getYoutubeLink())
-                .pageCount(post.getPageCount())
-                .fileNames(fileNames)
-                .comments(comments)
-                .feedbacks(feedbacks)
-                .build();
-
-        return successResponse(CustomMessage.OK, result);
+    private List<ResCommentDto> commentList(Post post, Member myMember) {
+        return commentService.findAllCommentsOfPost(post).stream()
+                .filter(comment -> comment.getParentComment() == null)
+                .map(comment -> ResCommentDto.builder()
+                        .commentId(comment.getCommentId())
+                        .isLiked(isLikedComment(myMember.getMemberId(), comment))
+                        .member(new ResMemberInfoDto(comment.getMember().getMemberId(),
+                                comment.getMember().getNickname(),
+                                comment.getMember().getProfileImage(),
+                                isMymMemberFollowMember(myMember, post.getMember())))
+                        .comment(comment.getComment())
+                        .likeCount(comment.getCommentLikeCount())
+                        .commentedTime(comment.getCommentedTime())
+                        //아래부터 대댓글 comment 조회 및 dto 매핑
+                        .replies(commentService.getParentCommentsReply(comment).stream()
+                                .map(reply -> ResReplyDto.builder()
+                                        .replyId(reply.getCommentId())
+                                        .member(new ResMemberInfoDto(reply.getMember().getMemberId(),
+                                                reply.getMember().getNickname(),
+                                                reply.getMember().getProfileImage(),
+                                                isMymMemberFollowMember(myMember, post.getMember())))
+                                        .content(reply.getComment())
+                                        .likeCount(reply.getCommentLikeCount())
+                                        .isLiked(isLikedComment(myMember.getMemberId(), reply))
+                                        .repliedTime(reply.getCommentedTime())
+                                        .build()).collect(Collectors.toList()))
+                        .build()).collect(Collectors.toList());
     }
 }
