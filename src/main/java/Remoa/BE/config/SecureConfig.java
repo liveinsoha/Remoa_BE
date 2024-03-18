d1a87a9ec9352420f7add4e119ecdb7817f11232
JJangcoding <bymyself103@naver.com>
2024-03-18 19:37:12 +0900

    commit for project

--- src/main/java/Remoa/BE/config/SecureConfig.java
+++ src/main/java/Remoa/BE/config/SecureConfig.java
@@ -1,62 +1,48 @@
 package Remoa.BE.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.CorsFilter;
 
-@RequiredArgsConstructor
-@EnableWebSecurity
 @Configuration
-public class SecureConfig {
+@EnableWebSecurity
+public class SecureConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin1234"))
+                .roles("ADMIN");
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").hasRole("ADMIN") // 다른 요청에는 인증이 필요
+                .and()
+                .formLogin() // 로그인 페이지 설정
+                .permitAll() // 로그인 페이지에는 모든 사용자가 접근할 수 있어야 함
+                .and()
+                .logout()
+                .permitAll() // 로그아웃 페이지에는 모든 사용자가 접근할 수 있어야 함
+                .and()
+                .sessionManagement()
+                .maximumSessions(-1) // 세션 제한 없음
+                .maxSessionsPreventsLogin(false) // 중복 접속시 마지막 세션만 유지
+                .and();
+        http.csrf().disable(); // CSRF 보안 비활성화
+    }
 
-    /**
-     * Spring Security에서 사용할 password encoder로 BCryptPasswordEncoder 지정
-     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    /**
-     * Spring Security 설정
-     */
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-
-        http.formLogin().disable();
-
-        http
-                .authorizeRequests()
-                //api 명세 확정 후 재확인 핋요
-                .antMatchers("/**").permitAll()
-                /*.antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/logout").permitAll()
-                .antMatchers("/upload").permitAll()
-                .antMatchers("/download").permitAll()
-                .antMatchers("/signup/**").permitAll()
-                .antMatchers("/mypage").authenticated()*/
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .and()
-
-                .sessionManagement()
-                .maximumSessions(-1) //세션 제한 없음
-                .maxSessionsPreventsLogin(false); //중복 접속시 마지막 세션만 유지
-
-        http.csrf().disable();
-
-        return http.build();
-    }
-
 }
